eac35d72b2506d449a518113a95d45f8b5300936
burletov <burletov@bk.ru>
2021-03-02 13:35:44 +0300

    feat: implement check(str, bracketsConfig) function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if(typeof str !== 'string'){
+        return false;
+    }
+
+    let result = [],
+        open = [],
+        close = [],
+        chars = str.split('');
+
+    for(let i = 0; i < bracketsConfig.length; i++){
+        open.push(bracketsConfig[i][0]);
+        close.push(bracketsConfig[i][1]);
+    }
+
+    for (let i = 0; i < chars.length; i++) {
+        let openIndex = open.indexOf(chars[i]);
+
+        if (openIndex !== -1 && close.indexOf(chars[i]) === -1){
+            result.push(openIndex);
+            continue;
+
+        }
+        if (openIndex !== -1 && close.indexOf(chars[i]) !== -1 && result.indexOf(openIndex) === -1){
+            result.push(openIndex);
+            continue;
+
+        }
+
+        let closeIndex = close.indexOf(chars[i]);
+        if (closeIndex !== -1) {
+            openIndex = result.pop();
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+        }
+    }
+
+    return result.length === 0;
+
 }
+
